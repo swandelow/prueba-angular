3f231401d9a806d036d717beff693d11c095c006
swandelow <swandelow@despegar.com>
2016-09-28 17:57:51 -0300

    agrego métodos algunos métodos a la app.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, Response, json, request, url_for
 
 app = Flask(__name__)
 
@@ -10,6 +10,36 @@ def prueba_1():
 def prueba_2():
 	return send_file('application2.html')
 
+todos = []
+count = 0
+
+@app.route('/api/todos', methods = ['GET'])
+def fetch_all():
+	resp = Response(json.dumps(todos), status=200, mimetype='application/json')
+	return resp
+
+@app.route('/api/todos', methods = ['POST'])
+def create():
+	global count
+	if request.headers['Content-Type'] == 'application/json':
+		todo = request.json
+		todo['id'] = count
+		count += 1
+		todos.append(todo)
+		resp = Response(status=201)
+		resp.headers['Location'] = url_for('fetch', id=todo['id'])
+	else:
+		resp = Response(status=500)
+	return resp
+
+@app.route('/api/todos/<int:id>')
+def fetch(id):
+	resp = Response(status=404)
+	for todo in todos:
+		if todo['id'] == id:
+			resp = Response(json.dumps(todo), status=200, mimetype='application/json')
+
+	return resp
 
 if __name__ == "__main__":
     app.run(debug=True)
