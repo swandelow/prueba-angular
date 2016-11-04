d3e50986e7b8dd26f7737e339cc430eaf8801c71
Sebass <swandelow@gmail.com>
2016-11-04 11:30:09 -0300

    jugando

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, send_file, Response, json, request, url_for
 
 app = Flask(__name__)
 
+LOGGER = app.logger
+
+@app.before_request
+def log_request_info():
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', request.get_data())
+
 @app.route('/prueba1')
 def prueba_1():
 	return send_file('application.html')
@@ -20,16 +27,23 @@ def fetch_all():
 
 @app.route('/api/todos', methods = ['POST'])
 def create():
+	LOGGER.debug('Creating a new todo.')
 	global count
-	if request.headers['Content-Type'] == 'application/json':
+	if request.headers['Content-Type'] == 'application/json;charset=UTF-8':
+		LOGGER.debug('Getting request data...')
 		todo = request.json
+		LOGGER.debug(todo)
 		todo['id'] = count
 		count += 1
 		todos.append(todo)
 		resp = Response(status=201)
 		resp.headers['Location'] = url_for('fetch', id=todo['id'])
 	else:
-		resp = Response(status=500)
+		error = {
+			'code' : '001',
+			'message' : 'BAD REQUEST!'
+		}
+		resp = Response(json.dumps(error), status=500)
 	return resp
 
 @app.route('/api/todos/<int:id>')
--- static/js/application.js
+++ static/js/application.js
@@ -10,22 +10,37 @@ App.config(['uibDatepickerConfig', 'uibDatepickerPopupConfig', function(uibDatep
     uibDatepickerPopupConfig.onOpenFocus = false;
 }]);
 
-App.controller('ApplicationController', function($scope, $filter) {
+App.controller('ApplicationController', function($scope, $filter, $resource) {
     $scope.data = { 'foo': 'bar' };
     $scope.savedObjects = [];
+    $scope.todo = {};
 
     function formatDate(date) {
         return $filter('date')(date, 'dd/MM/yyyy');
     }
 
+    var Todos = $resource('/api/todos/:todoId', 
+                            {todoId:'@id'},
+                            {
+                                post: {method: 'POST'},
+                                get: {method: 'GET'},
+                                getAll: {method: 'GET', isArray: true}
+                            });
+
     $scope.save = function() {
         if ($scope.form.$valid) {
             var obj = {
-                name: $scope.name,
-                period: { from: formatDate($scope.period.from), to: formatDate($scope.period.to) }
+                name: $scope.todo.name,
+                period: { from: formatDate($scope.todo.period.from), to: formatDate($scope.todo.period.to) }
             };
 
             $scope.savedObjects.push(obj);
+            Todos.post($scope.todo).$promise
+                            .then(function(response){
+                                console.log('saved:' + response.data);
+                            }, function(error){
+                                console.log('error: ' + error.data.code + ' - '+ error.data.message);
+                            });
         } else {
             for (i in $scope.form.$error.required) {
                 console.log($scope.form.$error.required[i].$name);
